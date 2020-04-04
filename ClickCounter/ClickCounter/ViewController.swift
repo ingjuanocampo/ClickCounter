eaf7250034ac72541522747e24e4f0d4d8966643
Juan Manuel Ocampo Valencia <juano@hourly.app>
2020-04-04 11:29:11 -0500

    Initial setup with storyboard

---
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by juan ocampo on 4/4/20.
+//  Copyright © 2020 juan ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
