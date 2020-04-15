7092e63a544ee1f6f5c0c7369e92fe8d9d36573c
Juan Manuel Ocampo Valencia <juano@hourly.app>
2020-04-15 16:18:42 -0500

    First table ui example

--- ClickCounter/ClickCounter/Tables/TableController.swift
+++ ClickCounter/ClickCounter/Tables/TableController.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+
+import UIKit
+
+class TableController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    let favoriteThings: [CellDataSruct] = [
+        CellDataSruct(title: "Apple", subTitle: "Fruit"),
+        CellDataSruct(title: "Red", subTitle: "Color"),
+        CellDataSruct(title: "Car" , subTitle: "Thing"),
+        CellDataSruct(title: "Android", subTitle: "Operate System"),
+        CellDataSruct(title: "Green" , subTitle: "Color"),
+        CellDataSruct(title: "Bike" , subTitle: "Thing")
+    ]
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteThings.count
+    }
+    
+    // cell for row at index path
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+      let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteThingCell")
+        let cellData = favoriteThings[indexPath.row]
+        cell?.textLabel?.text = cellData.title
+        cell?.detailTextLabel?.text = cellData.subTitle
+      return cell!
+    }
+    
+    
+    struct CellDataSruct {
+        let title: String
+        let subTitle: String
+    }
+    
+}
