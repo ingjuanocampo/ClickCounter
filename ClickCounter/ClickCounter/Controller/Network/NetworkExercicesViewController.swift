17f3e79c7578a02a7852ce84b15f52ca0eef85f5
juan ocampo <ing.juanocampo@gmail.com>
2020-05-01 19:45:08 -0500

    Organizing the project, adding networking test ui

---
+++ ClickCounter/ClickCounter/Controller/Network/NetworkExercicesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkExamplesViewController.swift
+//  ClickCounter
+//
+//  Created by juan ocampo on 5/1/20.
+//  Copyright © 2020 juan ocampo. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class NetworkExercicesViewController: UIViewController {
+        
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func onRequestPressed(_ sender: Any) {
+        
+    }
+}
